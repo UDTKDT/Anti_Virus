e06e93a27403f2a755ae44a4a9b12cef6d2ddd6a
hun <chamgf5247@naver.com>
2024-05-15 14:09:02 +0900

    t

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 void CheckOpt(int argc, char** argv){
     int optionIndex= 0;
     int opt;
-    const char* option="hibs"; //인자값 필요로 한다면 :붙이기 ex) hib:s:
+    const char* option="hib:s"; //인자값 필요로 한다면 :붙이기 ex) hib:s:
 
     while((opt = getopt_long(argc, argv, option, options, &optionIndex)) != -1 ){
         switch(opt){
@@ -23,7 +23,7 @@ void CheckOpt(int argc, char** argv){
                 break;
 
             case 'b':
-                background();
+                background(argv[2]);
                 break;
             
             case 's':
@@ -31,7 +31,7 @@ void CheckOpt(int argc, char** argv){
                 break;
 
             case '?':
-                cout << "Error" << endl;
+                error();
                 break;
 
             default:
@@ -42,6 +42,7 @@ void CheckOpt(int argc, char** argv){
 }
 
 int main(int argc, char **argv){
+
 
     //옵션 값 확인
     if (argc > 1) 
--- main.h
+++ main.h
@@ -1,8 +1,12 @@
 #include <getopt.h>
 #include <iostream>
+#include <cstring>
 #include <sys/ptrace.h>
 #include <chrono>
-#include <thread>
+#include <vector>
+#include <dirent.h>
+#include <fstream>
+#include <unistd.h>
 
 using namespace std;
 
@@ -10,7 +14,7 @@ using namespace std;
 struct option options[]={
     {"help", no_argument, 0, 'h'},
     {"info", no_argument, 0, 'i'},
-    {"background",no_argument, 0, 'b'},
+    {"background",required_argument, 0, 'b'},
     {"scan", no_argument, 0,'s'}, //인자값 필요로 한다면 no_argument -> required_argument
     {0,0,0,0}
 };
@@ -44,14 +48,41 @@ void info(){
     cout << "This tool is essential for maintaining optimal security in vulnerable or targeted environments, providing users with peace of mind through defensive capabilities." << endl;
 }
 
+void error(){
+    cout << "error" << endl;
+}
+
+
 int logic1(){
+    DIR* dir;
+    struct dirent *entry;
+
+    dir = opendir("/proc");
+
+    while((entry = readdir(dir)) != nullptr){
+        string path = "/proc/" + string(entry->d_name);
+        
+        if (entry->d_type == DT_DIR && string(entry->d_name).find_first_not_of("0123456789") == string::npos){
+            string comm_path = path+"/comm";
+            ifstream comm_file(comm_path);
+            string process_name;
+
+            if (comm_file >> process_name){
+                cout << "process ID : " << entry->d_name << " process Name : " << process_name << endl;
+            }
+            comm_file.close();
+        }
+
+    }
+    closedir(dir);
+    
     return 0;
 }
 
 int logic2(){
     if (ptrace(PTRACE_TRACEME, 0, 1, 0) == -1)
     {
-        cout << "don't trace me !!" <<endl;
+        cout << "don't trace me !!"  << endl;
         return 1;
     }
 
@@ -59,13 +90,21 @@ int logic2(){
     return 0;
 }
 
-void background(){
-    while (1){
-        cout << "Anti-debugging Logic Running…" << endl;
+void background(char* argv){
 
+    if(strcmp(argv, "logic")==0){
         logic1();
-        logic2();
-        this_thread::sleep_for(chrono::seconds(1));
-
     }
+    // while (1){
+    //     cout << "Anti-debugging Logic Running…" << endl;
+
+    //     logic1();
+    //     // logic2();
+    //     sleep(1);
+
+    // }
+
+    
+
+
 }
